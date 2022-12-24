529af1ceeb35f8d2f1737d2f7443e356fe7421d8
bagus-net <you@example.com>
2022-12-24 11:09:13 +0700

    dump

--- db/db-koperasi-laravel.sql
+++ db/db-koperasi-laravel.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : DBKU
+ Source Server         : dbku
  Source Server Type    : MySQL
  Source Server Version : 100422
  Source Host           : localhost:3306
@@ -11,7 +11,7 @@
  Target Server Version : 100422
  File Encoding         : 65001
 
- Date: 21/12/2022 13:50:52
+ Date: 24/12/2022 11:09:01
 */
 
 SET NAMES utf8mb4;
@@ -31,11 +31,12 @@ CREATE TABLE `member_models`  (
   `created_at` datetime(0) NULL DEFAULT NULL,
   `updated_at` datetime(0) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of member_models
 -- ----------------------------
+INSERT INTO `member_models` VALUES (2, 'baru', 'grogol', '08972739', 'bagusse@gmail.com', 'aktif', '2022-12-24 02:41:57', '2022-12-24 02:41:57');
 
 -- ----------------------------
 -- Table structure for product_category_models
@@ -48,12 +49,12 @@ CREATE TABLE `product_category_models`  (
   `created_at` datetime(0) NULL DEFAULT NULL,
   `updated_at` datetime(0) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of product_category_models
 -- ----------------------------
-INSERT INTO `product_category_models` VALUES (1, 'makanan', 'jajan', '2022-12-21 07:24:01', '2022-12-21 07:24:01');
+INSERT INTO `product_category_models` VALUES (1, 'buku', 'ada', '2022-12-24 02:38:16', '2022-12-24 02:38:16');
 
 -- ----------------------------
 -- Table structure for product_models
@@ -71,12 +72,17 @@ CREATE TABLE `product_models`  (
   `created_at` datetime(0) NULL DEFAULT NULL,
   `updated_at` datetime(0) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of product_models
 -- ----------------------------
-INSERT INTO `product_models` VALUES (1, '1', 'jajan1', 'jajanans ehat', NULL, 5000, 2, 'ada', '2022-12-21 13:36:56', '2022-12-21 13:36:58');
+INSERT INTO `product_models` VALUES (1, '1', 'sinar dunia', 'sinar dunia 50 lbr', '', 5000, 5, 'ada', '2022-12-24 02:38:52', '2022-12-24 02:38:52');
+INSERT INTO `product_models` VALUES (2, '2', 'pensil', 'pensil 2b', '', 3000, 90, 'ada', NULL, NULL);
+INSERT INTO `product_models` VALUES (3, NULL, 'komo', 'jajan komo', '', 3000, 90, 'ada', NULL, NULL);
+INSERT INTO `product_models` VALUES (4, NULL, 'kacang', 'kacang garuda', '', 10000, 90, 'ada', NULL, NULL);
+INSERT INTO `product_models` VALUES (5, '3', 'kacang', 'kacang garuda', '', 10000, 90, 'ada', NULL, NULL);
+INSERT INTO `product_models` VALUES (6, '3', 'kacang', 'kacang garuda', '', 10000, 90, 'ada', NULL, NULL);
 
 -- ----------------------------
 -- Table structure for voucher_models
@@ -90,10 +96,11 @@ CREATE TABLE `voucher_models`  (
   `created_at` datetime(0) NULL DEFAULT NULL,
   `updated_at` datetime(0) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of voucher_models
 -- ----------------------------
+INSERT INTO `voucher_models` VALUES (1, 'mahasiswa', '5%', 'ada', '2022-12-24 02:40:08', '2022-12-24 02:40:08');
 
 SET FOREIGN_KEY_CHECKS = 1;
